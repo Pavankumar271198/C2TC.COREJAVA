2a9b9834768fcfdcb9edd5f80a02d2cf50c33176
Admin <Admin@DESKTOP-RC4PBRV>
2022-05-10 19:06:10 -0800

    FIRST COMMIT

---
+++ array1/src/array1/array1.java
@@ -0,0 +1,25 @@
+package array1;
+import java.util.Scanner;
+
+public class array1 {
+
+	public static void main(String[] args) {
+		int n,sum = 0;
+		Scanner s1=new Scanner(System.in);
+		System.out.println("Enter number of elements you want in array:");
+		n=s1.nextInt();
+		int a[]= new int[n];
+		System.out.println("Enter the elements of array:");
+		for(int i = 0;i < n; i++)
+		{ 
+			
+			a[i]=s1.nextInt();
+			sum = sum +a[i];
+		}
+		System.out.println("Sum:"+sum);
+	}
+		
+		
+
+
+}
