e1898c33d93fb6b8f29fafc8c9347eb773b96aef
Admin <Admin@DESKTOP-RC4PBRV>
2022-05-10 20:45:55 -0800

    FIRST COMMIT

---
+++ EVENODD1/src/evenodd1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class evenodd1 {
+
+	public static void main(String[] args) {
+		Scanner s1=new Scanner(System.in);
+		System.out.println("enter the number");
+		int number=s1.nextInt();
+		if((number&1)==1)
+			System.out.println(number+"is odd");
+		else
+			System.out.println(number+"is even");
+		s1.close();
+
+	}
+
+}
+
